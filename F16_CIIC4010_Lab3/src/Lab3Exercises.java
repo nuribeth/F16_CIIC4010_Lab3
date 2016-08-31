0e797e3119e7ef98d286adc9ae15fc38b1f50b07
4015b25 <4015b25@tamarindo.ece.uprm.edu>
2016-08-31 10:37:32 -0400

    Change my name

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame nuribeth = new JFrame("Hello, world!");
+		JFrame nuribeth = new JFrame("Hello, Nuri!");
 		nuribeth.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		nuribeth.setLocation(400, 250);
 		nuribeth.setSize(200, 200);
